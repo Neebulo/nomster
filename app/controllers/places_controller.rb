d6a5ede8dfffa599817c50bd6291cc71b0892578
neebulo <rhapsodyshadow@yahoo.com>
2017-06-22 20:28:35 +0000

    places_controller.rb | added before_action filter to allow only logged in NEW and CREATE

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @places = Place.order(:name).page(params[:page]).per(3)
