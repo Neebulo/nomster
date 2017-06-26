3daf661124b4e894bb3790d628e131c0693d92fd
neebulo <rhapsodyshadow@yahoo.com>
2017-06-26 06:09:42 +0000

    place.rb | more validation for :name, :address, :description

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,8 @@ class Place < ApplicationRecord
   paginates_per  5
 
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 4 }
+  validates :address, presence: true
+  validates :description, presence: true
 
 end
