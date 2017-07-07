7837f5f069ad71175f3d3846117451af4d46cfd0
neebulo <rhapsodyshadow@yahoo.com>
2017-07-07 03:35:50 +0000

    comments_controller.rb | auth user before create action, create action to load place where comment belongs via params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+before_action :authenticate_user!
+
+  def create
+    @place = Place.find(params[:place_id])
+  end
+
+
+
+
+
+
+
+
+
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:message, :rating)
+  end
 end
